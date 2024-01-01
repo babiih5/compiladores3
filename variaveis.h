ed6c32aba998d62a384f6e3229454b27398b0ba2
Bárbara Ferreira <barbara9ferreira@gmail.com>
2024-01-01 00:15:40 +0000

    comentarios

--- variaveis.h
+++ variaveis.h
@@ -1,6 +1,8 @@
 #ifndef VARIAVEIS_H
 #define VARIAVEIS_H
 
+/*declaração das variáveis a usar no lex e no yacc*/
+
 extern int manutencao, estado_bateria, quantidade, num_manutencao, tarefas, total_quantidade;
 extern char *instrucao, *posicao, *linha, *quant_str, *material_entrega, *material_carro,*material_recolha,*aux_material, *aux_estado, *total_materiais;
 extern float perc_bateria,aux_quant,bateria_necessaria;
